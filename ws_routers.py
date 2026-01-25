bb7850113860bb1cc532048f4609898660f7b84c
Quang <nguyenquangpen@gmail.com>
2026-01-25 16:25:21 +0700

    change solution load florence model

--- ws_routers.py
+++ ws_routers.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI, WebSocket, APIRouter
 import uvicorn
 import base64
 import json
-from utils.helpers import *
+import asyncio
 from Llama_3.view import LlamaView
 from Florence.view import FlorenceView
 from Florence.model import FlorenceInput, FlorenceOutput
@@ -10,6 +10,9 @@ from Florence.model import FlorenceInput, FlorenceOutput
 app = FastAPI()
 florence_view = FlorenceView()
 llama_view = LlamaView()
+
+model_lock = asyncio.Lock()
+model_loaded = False
 
 @app.websocket("/ws/agent")
 async def video_handler(websocket: WebSocket):
@@ -19,14 +22,20 @@ async def video_handler(websocket: WebSocket):
         while True:
             msg = await websocket.receive_text()
             if msg == "init_florence":
-                # setup florence model here
-                florence_view._load_model()
+                async with model_lock:
+                    if not model_loaded:
+                        await asyncio.to_thread(florence_view._load_model)
+                        model_loaded = True
+                        
+                print("Florence-2 model is ready")
                 await websocket.send_text("ready_florence")
                 continue
             
             elif msg == "init_llama":
-                # setup llama model here
-                # llama_view._load_model()
+                # async with model_lock:
+                #     if not model_loaded:
+                #         await asyncio.to_thread(llama_view._load_model)
+                #         model_loaded = True
                 await websocket.send_text("ready_llama")
                 continue
             
@@ -42,7 +51,7 @@ async def video_handler(websocket: WebSocket):
             
             try:
                 data = json.loads(msg)
-                if data.get("Status") == "run":
+                if data.get("Status") == "run_florence":
                     shot_id = data.get("shot_id")
                     image_b64 = data.get("image_b64")
                     print(f"Processing shot: {shot_id}")
@@ -51,11 +60,9 @@ async def video_handler(websocket: WebSocket):
                         shot_id=shot_id,
                         image_b64=image_b64
                     )
-
                     print("Generating caption with Florence-2...")
                     results = florence_view.generate_caption(input_obj)
                     print("Caption generated")
-
                     if results:
                         response = {
                             "shot_id": shot_id,
